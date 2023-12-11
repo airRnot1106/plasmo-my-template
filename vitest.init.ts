cfe6d2e7002de684cd9d419722b015d5da155473
airRnot1106 <yngw.rintaro@gmail.com>
2023-12-11 23:31:07 +0900

    add storybook mock for vitest

--- vitest.init.ts
+++ vitest.init.ts
@@ -2,3 +2,8 @@ import '@testing-library/jest-dom';
 import * as chrome from 'vitest-chrome/lib/index.esm';
 
 Object.assign(global, chrome);
+
+import { setProjectAnnotations } from '@storybook/react';
+import * as globalStorybookConfig from './.storybook/preview';
+
+setProjectAnnotations(globalStorybookConfig.default);
