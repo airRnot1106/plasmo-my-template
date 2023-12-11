6a29e035fa4ca36879afb64ed864e5409b8bb352
airRnot1106 <yngw.rintaro@gmail.com>
2023-12-11 23:06:12 +0900

    add vitest

---
+++ index.d.ts
@@ -0,0 +1,1 @@
+declare module 'vitest-chrome/lib/index.esm';
---
+++ vitest.init.ts
@@ -0,0 +1,4 @@
+import '@testing-library/jest-dom';
+import * as chrome from 'vitest-chrome/lib/index.esm';
+
+Object.assign(global, chrome);
